c9d38615a79ff6cef97f89c2a07bb8a4303d8b59
kevinhuang-max <kevin.huang@visitingmedia.com>
2025-12-11 15:23:33 -0500

    Fix firecrawl import

--- app/api/scrape/route.js
+++ app/api/scrape/route.js
@@ -1,4 +1,4 @@
-import FirecrawlApp from '@firecrawl/firecrawl-js';
+import FirecrawlApp from '@mendable/firecrawl-js';
 
 const firecrawl = new FirecrawlApp({ apiKey: process.env.FIRECRAWL_API_KEY });
 
